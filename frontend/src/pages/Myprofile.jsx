dea50fbe57037abe9dd0f4687039d06d04d3f70b
Nawal <maham22.sultan2002@gmail.com>
2026-06-07 02:12:48 +0500

    frontend

--- frontend/src/pages/Myprofile.jsx
+++ frontend/src/pages/Myprofile.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import assets from '../assets/assets_frontend/assets'
 
 const Myprofile = () => {
+
+
+ const [userData, setuserData] = useState({ 
+  name:"Edward Vincent",
+  image:assets.profile_pic,
+  email:'nawal03.sultan2004@gmail.com',
+  phone:"+1 123 456 789 ",
+  address:{
+    line1:"57th Cross, Richmond ",
+    line2:"Circle Curhch Road, London"
+  },
+  gender:'Male',
+  Dob:'2000-01-20'
+})
+
+const [isEdit, setisEdit] = useState(false)
+
+
   return (
-    <div>Myprofile</div>
+    <div>
+      <img src={userData.image} alt="" />
+
+      {
+        isEdit
+        ? <input type='text' value={userData.name} onChange={e=>setuserData(prev =>({...prev,name:e.target.value }))} />
+        : <p>{userData.name}</p>
+
+      }
+      <hr />
+      <div>
+        <p>CONTACT INFORMATION</p>
+        <div>
+          <p>Email id:</p>
+          <p>{userData.email}</p>
+          <p>phone:</p>
+          
+      {
+        isEdit
+        ? <input type='text' value={userData.phone} onChange={e=>setuserData(prev =>({...prev,phone:e.target.value }))} />
+        : <p>{userData.phone}</p>
+
+      }
+        </div>
+      </div>
+    </div>
   )
 }
 
